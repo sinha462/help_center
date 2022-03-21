4d295a448752f207280ee3ffe50340249cec40ee
sinha462 <60381958+sinha462@users.noreply.github.com>
2022-03-21 21:54:29 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+import Girl from "./image1.png";
+
+function App() {
+  return (
+    <div className="App">
+      <div class="container">
+        <div class="e1_805">
+          <div class="e1_806">
+            <span class="e1_807">Hi, How can we help?</span>
+            <div className="e1_808">
+              <img src={Girl} alt="" width="100%" />
+            </div>
+            <div class="e1_855"></div>
+            <div class="e1_856">
+              <div class="e1_857"></div>
+              <div class="e1_858"></div>
+            </div>
+            <span class="e1_859">Help Center</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
